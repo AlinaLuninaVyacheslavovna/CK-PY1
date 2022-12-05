bbffbe1e06caf054d72e1828664e830a9a95617e
AlinaLuninaVyacheslavovna <118982783+AlinaLuninaVyacheslavovna@users.noreply.github.com>
2022-12-05 19:33:22 +0300

    Update task3.py

--- Лабораторная работа №4/task3.py
+++ Лабораторная работа №4/task3.py
@@ -1,6 +1,8 @@
 def delete(list_, index=None):
-    if index==None:
+    if index==None or index==-1:
         return list_[:-1]
+    elif index < 0:
+        return list_[index - 1::-1][::-1] +list_[-1:index:-1][::-1]
     return list_[:index] + list_[index + 1:]
 
 print(delete([0, 1, 2], index=0))  # [0, 1]
